4cd6dbb22feaf79686efbc4eb72994ce10424990
Malek Cellier <malek.cellier@gmail.com>
2019-07-09 09:41:31 +0200

    fix attribute exposition

--- gui/gui_topology.py
+++ gui/gui_topology.py
@@ -6,24 +6,27 @@ class GuiPositions(QObject):
     def __init__(self, position, parent=None):
         super().__init__(parent)
         self.position = position
+        self._x = [float(n) for n in self.position.x]
+        self._y = [float(n) for n in self.position.y]
 
     @Property('QVariant')
     def x(self):
-        return self.position.x
+        return self._x
 
     @Property('QVariant')
     def y(self):
-        return list(self.position.y)
+        return self._y
 
 
 class GuiNodes(QObject):
     def __init__(self, nodes, parent=None):
         super().__init__(parent)
         self.nodes = nodes
+        self._positions = [GuiPositions(positions) for positions in self.nodes.positions]
 
     @Property('QVariant')
     def positions(self):
-        return [GuiPositions(positions) for positions in self.nodes.positions]
+        return self._positions
 
 
 class GuiTopology(QObject):
@@ -33,6 +36,7 @@ class GuiTopology(QObject):
     def __init__(self, preset, parent=None):
         super().__init__(parent)
         self.topology = Topology(preset)
+        self._nodes = [GuiNodes(nodes) for nodes in self.topology.nodes.values()] 
 
     @Property(str)
     def presetName(self):
@@ -40,5 +44,5 @@ class GuiTopology(QObject):
 
     @Property('QVariant')
     def nodes(self):
-        return [GuiNodes(nodes) for nodes in self.topology.nodes.values()]
+        return self._nodes
 
